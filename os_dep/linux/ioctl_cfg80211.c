810f27ff8732a43d127923598ccaa42288f9f72a
Preston Smith <preston.s1993@gmail.com>
2019-07-05 19:49:02 -0500

    Fix Linux 5.1

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -338,7 +338,7 @@ static u64 rtw_get_systime_us(void)
 	struct timespec64 ts;
 	ktime_get_boottime_ts64(&ts);
 	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
-#elseif (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
 	struct timespec ts;
 	get_monotonic_boottime(&ts);
 	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
